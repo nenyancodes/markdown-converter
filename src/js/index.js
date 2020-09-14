4e85e365ba2c307880be0f0b1f5ef48814b4e432
Ania Wójtowicz <wojtowicz.anna.maria+nenyan@gmail.com>
2020-09-14 16:51:07 +0200

    Rendered markdown

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,49 @@
 import "../scss/main.scss";
+import MarkdownIt from "markdown-it";
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
-
-/* place your code below */
+const markdownIt = new MarkdownIt();
 
 console.log("HELLO 🚀");
+
+localStorage.setItem("entry", "");
+
+let result = "";
+const entry = localStorage.getItem("entry");
+
+if (entry) {
+  result = entry;
+}
+
+const textarea = document.querySelector(".markdown__textarea");
+textarea.value = result;
+
+const converted = document.querySelector(".converted");
+
+const renderMarkdown = () => {
+  console.log("render");
+
+  let text = localStorage.getItem("entry");
+  console.log(text);
+  let textConverted = markdownIt.render(text);
+  converted.innerHTML = `<div>${textConverted}</div>`;
+  console.log(textConverted);
+};
+
+const saveButton = document.querySelector(".markdown__button-save");
+saveButton.addEventListener("click", () => {
+  localStorage.setItem("entry", textarea.value);
+  renderMarkdown();
+});
+
+const clearButton = document.querySelector(".markdown__button-clear");
+clearButton.addEventListener("click", () => {
+  textarea.value = "";
+  localStorage.setItem("entry", textarea.value);
+  renderMarkdown();
+});
+
+const loadButton = document.querySelector(".markdown__button-load");
+loadButton.addEventListener("click", () => {
+  textarea.value = localStorage.getItem("entry");
+  renderMarkdown();
+});
